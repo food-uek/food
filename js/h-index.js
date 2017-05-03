3dc1b1a7677c5fad41e3031776c4cb7cf76ad55f
651619709@qq.com <651619709@qq.com>
2017-05-03 09:03:22 +0800

    H-v-1.4

--- js/h-index.js
+++ js/h-index.js
@@ -2,15 +2,14 @@ $(document).ready(function(){
 	var myscroll=new IScroll("#h-iscroll",{
 		mouseWheel:true,
 		scrollbars:false,
-		
+		click:true,
 	});
 	var mySwiper = new Swiper ('.h-bannerbox',{
 		pagination:'.h-nav',
 		
 	})
-	var mySwiper = new Swiper ('.h-cookBox',{
+	var mySwiper_2 = new Swiper ('.h-cookBox',{
 		pagination:false,
 		slidePerView:2,
-		spaceBetween:10,
 	})
 })
