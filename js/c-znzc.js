ced57dab30cb725cbd7650301484f626985e5c83
QianRuoxi <914274917@qq.com>
2017-05-07 23:44:39 +0800

    'cy23'

--- js/c-znzc.js
+++ js/c-znzc.js
@@ -3,40 +3,59 @@
  * Created by Administrator on 2017/5/2.
  */
 $(function(){
-    //给每个菜添加属性‘cai’
-    // $('main >div >ul >li').forEach(function (value, index) {
-    //     $(value).attr('cai',index)
-    // });
-    var flag=true;
+
     // li的点击事件
     var arr=[];
-    $('main >div >ul >li').on('touchstart',function(){
+    $('main >div >ul >li').on('touchstart',function() {
+
         //获取li的菜名
-        var name=$(this).children('*:nth-child(3)').children('span').html();
+        var name = $(this).children('*:nth-child(3)').children('span').html();
         // 点击后给这个li下的 .dui 类名 active
         $(this).children('*:nth-child(2)').addClass('active');
-        // 如果开关打开就添加一个li
-        let that=$(this);
-        // console.log(that)
-        // if(flag){
 
-            arr.forEach(function (value,index) {
-                // if (!value==that.children('*:nth-child(3)').children('span').html()){
-                    arr.push(name);
+        var li=$(`<li type=${name}>`).html(`<span>${name}</span>
+        <img src="../img/c-img/cha.png">`);
 
-                // }
-                console.log(arr);
-            })
-            $(`<li type=${name}>`).html(`<span>${name}</span>
-            <img src="../img/c-img/cha.png">`).appendTo('.c-xuan');
-            flag=false;
-        // }
-
+        //把添加的li的type值放进arr中，如果arr里面不含有这个type值就让它添加进去
+        if(!arr.includes($(li).attr("type"))){
+            li.appendTo('.c-xuan');
+            arr.push($(li).attr("type"));
+        }
         // else{
-        //     flag=true;
+        //
+        //     $(this).children('*:nth-child(2)').removeClass('active');
         // }
 
-    })
+        // 如果添加的里的长度大于4，就off
+        if ($('.c-xuan>li').length >= 4) {
+            $('main >div >ul >li').off()
+        }
+
+
+
+        $('.c-xuan>li').on('touchstart',function () {
+            var typeval=$(this).attr('type');
+            $(this).remove();
+            $('main >div >ul >li').forEach(function (value,index) {
+                if($(value).children('*:nth-child(3)').children('span').html()==typeval){
+                    $('main >div >ul >li').eq(index).children('*:nth-child(2)').removeClass('active');
+                }
+            });
+
+
+            //将arr中对应的元素删除
+            var num=arr.findIndex(function () {
+                arr.forEach(function (val,index) {
+                    val==$(this).attr('type');
+                })
+            })
+            arr.splice(num,1)
+
+        })
+    });
+
+
+
 
 
 
