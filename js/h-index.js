28aa563e390b3b17401ae44c24cc5baa3d60d628
651619709@qq.com <651619709@qq.com>
2017-05-08 21:30:02 +0800

    H-V2.5

--- js/h-index.js
+++ js/h-index.js
@@ -20,5 +20,15 @@ $(document).ready(function(){
 		$('.q-footer').removeClass('q-active');
 		$('.q-zhao').removeClass('q-active');
 	})
+	$('.z-return').on('touchstart',function(){
+		$('.h-box-you').css({'left':'5.3rem','transform':'rotateY(-30deg)'});
+		$('.zl-zuo').css('left','0');
+	})
 	
+	var hammertime = new Hammer(document.querySelector(".zl-zuo"));
+	hammertime.on("swipeleft", function () {
+		$('.h-box-you').css({'left':'0','transform':'rotateY(0deg)'})
+ 		$('.zl-zuo').css('left','-5.8rem')
+	
+	});
 })
