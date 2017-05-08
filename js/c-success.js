2ba89245c598766e820fe81657a90ce6fbabcfc6
QianRuoxi <914274917@qq.com>
2017-05-08 13:40:59 +0800

    'ccyy'

--- js/c-success.js
+++ js/c-success.js
@@ -41,6 +41,15 @@ $(function () {
         $('.c-f-list >li').removeClass('first');
     });
 
-
+    //点击删除
+    $('.yx-list > .first').on('touchstart',function () {
+        var con=$(this).html();
+        $(this).remove();
+        $('.c-f-list >li').forEach(function (val,index) {
+            if($(val).html()==con){
+                $('.c-f-list >li').eq(index).removeClass('first');
+            }
+        })
+    })
 
 });
