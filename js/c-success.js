e0a60e5e2440388a2a376fe496ae8c5b03d0c4d6
QianRuoxi <914274917@qq.com>
2017-05-09 10:16:16 +0800

    'cy'

--- js/c-success.js
+++ js/c-success.js
@@ -12,27 +12,106 @@ $(function () {
         $('.zhe').css('display',"none");
     });
 
-    $('.c-s-con>div').forEach(function (val,index) {
-        var cl=$(val).attr('class');
-        $(`.${cl} .c-f-list >li`).on('touchstart',function () {
+    // $('.c-s-con >div').forEach(function (val,index) {
+    //     console.log($(val).attr('class'))
+    //     var cl=$(val).attr('class');
+    //     $(`.${cl} .c-f-list >li`).on('touchstart',function () {
+    //         $('.time >.c-f-list >li').removeClass('first');
+    //         $(this).addClass('first');
+    //         var yixuan=$(this).clone(true).attr('class',`${cl}list first`).append(`<img src="../img/c-img/cha.png" alt="">`);
+    //         $(`.${cl}list`).replaceWith(yixuan.appendTo($('.yx-list')));
+    //
+    //         //点击删除
+    //         $('.yx-list > .first').on('touchstart','img',function () {
+    //             var con=$(this).parent().children('span.c-x-text').html();
+    //             $(this).parent().remove();
+    //             $(`.${cl} .c-f-list >li .c-x-text`).forEach(function (val,index) {
+    //                 if($(val).html()==con){
+    //                     $(`.${cl}  .c-f-list >li`).eq(index).removeClass('first');
+    //                 }
+    //             })
+    //         })
+    //
+    //     });
+    // })
+
+        $(`.time .c-f-list >li`).on('touchstart',function () {
             $('.time >.c-f-list >li').removeClass('first');
             $(this).addClass('first');
-            var yixuan=$(this).clone(true).attr('class',`${cl}list first`).append(`<img src="../img/c-img/cha.png" alt="">`);
-            $(`.${cl}list`).replaceWith(yixuan.appendTo($('.yx-list')));
+            var yixuan=$(this).clone(true).attr('class','timelist first').append(`<img src="../img/c-img/cha.png" alt="">`);
+            $('.timelist').replaceWith(yixuan.appendTo($('.yx-list')));
 
             //点击删除
             $('.yx-list > .first').on('touchstart','img',function () {
                 var con=$(this).parent().children('span.c-x-text').html();
                 $(this).parent().remove();
-                $('.c-f-list >li').forEach(function (val,index) {
+                $('.time .c-f-list >li .c-x-text').forEach(function (val,index) {
                     if($(val).html()==con){
-                        $('.c-f-list >li').eq(index).removeClass('first');
+                        $('.time .c-f-list >li').eq(index).removeClass('first');
                     }
                 })
             })
 
         });
-    });
+        
+        $(`.step .c-f-list >li`).on('touchstart',function () {
+            $('.step >.c-f-list >li').removeClass('first');
+            $(this).addClass('first');
+            var yixuan=$(this).clone(true).attr('class','steplist first').append(`<img src="../img/c-img/cha.png" alt="">`);
+            $('.steplist').replaceWith(yixuan.appendTo($('.yx-list')));
+
+            //点击删除
+            $('.yx-list > .first').on('touchstart','img',function () {
+                var con=$(this).parent().children('span.c-x-text').html();
+                $(this).parent().remove();
+                $('.step .c-f-list >li .c-x-text').forEach(function (val,index) {
+                    if($(val).html()==con){
+                        $('.step .c-f-list >li').eq(index).removeClass('first');
+                    }
+                })
+            })
+
+        })
+
+        $(`.taste .c-f-list >li`).on('touchstart',function () {
+            $('.taste >.c-f-list >li').removeClass('first');
+            $(this).addClass('first');
+            var yixuan=$(this).clone(true).attr('class','tastelist first').append(`<img src="../img/c-img/cha.png" alt="">`);
+            $('.tastelist').replaceWith(yixuan.appendTo($('.yx-list')));
+
+            //点击删除
+            $('.yx-list > .first').on('touchstart','img',function () {
+                var con=$(this).parent().children('span.c-x-text').html();
+                $(this).parent().remove();
+                $('.taste .c-f-list >li .c-x-text').forEach(function (val,index) {
+                    if($(val).html()==con){
+                        $('.taste .c-f-list >li').eq(index).removeClass('first');
+                    }
+                })
+            })
+
+        });
+        $(`.tech .c-f-list >li`).on('touchstart',function () {
+            $('.tech >.c-f-list >li').removeClass('first');
+            $(this).addClass('first');
+            var yixuan=$(this).clone(true).attr('class','techlist first').append(`<img src="../img/c-img/cha.png" alt="">`);
+            $('.techlist').replaceWith(yixuan.appendTo($('.yx-list')));
+
+            //点击删除
+            $('.yx-list > .first').on('touchstart','img',function () {
+                var con=$(this).parent().children('span.c-x-text').html();
+                $(this).parent().remove();
+                $('.tech  .c-f-list >li .c-x-text').forEach(function (val,index) {
+                    if($(val).html()==con){
+                        $('.tech .c-f-list >li').eq(index).removeClass('first');
+                    }
+                })
+            })
+
+        });
+
+
+
 
     //清空
     $('.c-btn >.clear').on('touchstart',function () {
--- js/c-znzc.js
+++ js/c-znzc.js
@@ -3,62 +3,47 @@
  * Created by Administrator on 2017/5/2.
  */
 $(function(){
-
     // li的点击事件
     var arr=[];
+    var flag=true;
     $('main >div >ul >li').on('touchstart',function() {
+        // // 如果添加的里的长度大于4
+        if ($('.c-xuan>li').length > 3) {
+            return;
+        }
+        else {
+            //获取li的菜名
+            var name = $(this).children('*:nth-child(3)').children('span').html();
+            // 点击后给这个li下的 .dui 类名 active
+            $(this).children('*:nth-child(2)').addClass('active');
 
-        //获取li的菜名
-        var name = $(this).children('*:nth-child(3)').children('span').html();
-        // 点击后给这个li下的 .dui 类名 active
-        $(this).children('*:nth-child(2)').addClass('active');
-
-        var li=$(`<li type=${name}>`).html(`<span>${name}</span>
+            var li=$(`<li type=${name}>`).html(`<span>${name}</span>
         <img src="../img/c-img/cha.png">`);
 
-        //把添加的li的type值放进arr中，如果arr里面不含有这个type值就让它添加进去
-        if(!arr.includes($(li).attr("type"))){
-            li.appendTo('.c-xuan');
-            arr.push($(li).attr("type"));
-        }
-        // else{
-        //
-        //     $(this).children('*:nth-child(2)').removeClass('active');
-        // }
+            //把添加的li的type值放进arr中，如果arr里面不含有这个type值就让它添加进去
+            if(!arr.includes($(li).attr("type"))){
+                li.appendTo('.c-xuan');
+                arr.push($(li).attr("type"));
+            }
 
-        // 如果添加的里的长度大于4，就off
-        if ($('.c-xuan>li').length >= 4) {
-            $('main >div >ul >li').off()
-        }
+            $('.c-xuan>li').on('touchstart','img',function () {
+                var typeval=$(this).parent().attr('type');
+                $(this).parent().remove();
+                $('main >div >ul >li').forEach(function (value,index) {
+                    if($(value).children('*:nth-child(3)').children('span').html()==typeval){
+                        $('main >div >ul >li').eq(index).children('*:nth-child(2)').removeClass('active');
+                    }
+                });
+                //将arr中对应的元素删除
+                var num=arr.findIndex(function () {
+                    arr.forEach(function (val,index) {
+                        val==$(this).attr('type');
+                    })
+                });
+                arr.splice(num,1)
 
-
-
-        $('.c-xuan>li').on('touchstart','img',function () {
-            var typeval=$(this).parent().attr('type');
-            $(this).parent().remove();
-            $('main >div >ul >li').forEach(function (value,index) {
-                if($(value).children('*:nth-child(3)').children('span').html()==typeval){
-                    $('main >div >ul >li').eq(index).children('*:nth-child(2)').removeClass('active');
-                }
             });
-
-
-            //将arr中对应的元素删除
-            var num=arr.findIndex(function () {
-                arr.forEach(function (val,index) {
-                    val==$(this).attr('type');
-                })
-            })
-            arr.splice(num,1)
-
-        })
-    });
-
-
-
-
-
-
-
+        }
+    })
 
 });
