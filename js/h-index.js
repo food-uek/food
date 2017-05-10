357dfe65deb6641242a37cc110f06c21e378a677
651619709@qq.com <651619709@qq.com>
2017-05-10 09:36:23 +0800

    h-v4

--- js/h-index.js
+++ js/h-index.js
@@ -21,13 +21,13 @@ $(document).ready(function(){
 		$('.q-zhao').removeClass('q-active');
 	})
 	$('#h-list').on('touchstart',function(){
-		$('.h-box-you').toggleClass('h-active-1');
+		$('.h-box-you').css({left:'5.3rem',transform:'rotateY(-30deg)',transition:'all 0.5s'});
 		$('.zl-zuo').toggleClass('h-active-2');
 	})
 	
 	var hammertime = new Hammer(document.querySelector(".zl-zuo"));
 	hammertime.on("swipeleft", function () {
-		$('.h-box-you').removeClass('h-active-1');
+		$('.h-box-you').css({left:'0',transform:'rotateY(0)',transition:'all 0.5s'});
  		$('.zl-zuo').removeClass('h-active-2');
 	
 	});
--- js/h-two.js
+++ js/h-two.js
@@ -14,12 +14,12 @@ $(document).ready(function(){
 		pagination:'.h-nav2',
 	})
 	$('#h-list').on('touchstart',function(){
-		$('.h-box-you').toggleClass('h-active-1');
+		$('.h-box-you').css({left:'5.3rem',transform:'rotateY(-30deg)',transition:'all 0.5s'});
 		$('.zl-zuo').toggleClass('h-active-2');
 	})
 	var hammertime = new Hammer(document.querySelector(".zl-zuo"));
 	hammertime.on("swipeleft", function () {
-		$('.h-box-you').removeClass('h-active-1');
+		$('.h-box-you').css({left:'0',transform:'rotateY(0)',transition:'all 0.5s'});
  		$('.zl-zuo').removeClass('h-active-2');
 	});
 	
